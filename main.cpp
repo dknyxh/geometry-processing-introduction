621b6408e97a77914de3edae066005b963f1fe6d
KENAN DENG <dknyxh@gmail.com>
2018-01-16 22:57:06 -0500

    test on cube

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@
 #include <igl/per_face_normals.h>
 #include <igl/per_corner_normals.h>
 
+#include <igl/euler_characteristic.h>
+
 
   Eigen::MatrixXd N_vertices;
 Eigen::MatrixXd N_faces;
@@ -43,20 +45,21 @@ int main(int argc, char *argv[])
 
 
   // Load in a mesh
-  igl::read_triangle_mesh(argc>1 ? argv[1] : "../shared/data/bunny.off", V, F);
+  igl::read_triangle_mesh(argc>1 ? argv[1] : "../data/cube.off", V, F);
 
   Eigen::MatrixXi E = edges(F);
   int Chi = euler_characteristic(F);
   std::cout<<"Edge list E is "<<E.rows()<<"x"<<E.cols()<<std::endl;
-  std::cout<<"Euler Characteristic: "<<Chi<<std::endl;
-  std::cout<<"V"<<V <<std::endl;
+  std::cout<<"Euler Characteristic: "<<euler_characteristic(F)<<std::endl;
+  //std::cout<<"V"<<V <<std::endl;
   //std::cout<<"edge_flaps"<<igl::edge_flaps();
-  std::cout<<"F"<<F <<std::endl;
-  Eigen::MatrixXi o1,o2,o3;
-  igl::edge_topology(V, F, o1, o2, o3);
-  std::cout<<"edge_topology o1"<<o1 << std::endl;
-  std::cout<<"edge_topology o2"<<o2 << std::endl;
-  std::cout<<"edge_topology o3"<<o3 << std::endl;
+  //std::cout<<"F"<<F <<std::endl;
+  //Eigen::MatrixXi o1,o2,o3;
+  //igl::edge_topology(V, F, o1, o2, o3);
+  //std::cout<<"edge_topology o1"<<o1 << std::endl;
+  //std::cout<<"edge_topology o2"<<o2 << std::endl;
+  //std::cout<<"edge_topology o3"<<o3 << std::endl;
+
   // Create a libigl Viewer object 
   igl::viewer::Viewer viewer;
   // Set the vertices and faces for the viewer
